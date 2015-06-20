5d6f80c2901e01d89ce748b78a846831e447137b
Jacobo Dominguez <dominguezjaco@gmail.com>
2015-06-20 18:21:38 -0300

    Agregados DaoMenu, DaoPedido y DaoPlato

--- backend/app/dao/DaoBase.java
+++ backend/app/dao/DaoBase.java
@@ -4,7 +4,7 @@ import java.util.List;
  * Created by Pablo on 20/06/2015.
  */
 public interface DaoBase<T> {
-    public List<T> otener();
+    public List<T> obtener();
    /* public Boolean agregar(T objeto);
     public Boolean modificar(T objeto);
     public Boolean eliminar(T objeto);*/
