275f9d127ae4b40a6e1490ac5b0edb0d40d3b87f
Jacobo Dominguez <dominguezjaco@gmail.com>
2015-06-20 17:30:16 -0300

    Agregadas las primeras configuraciones para iniciar la app en el servidor.

---
+++ backend/config/servlet/ServletContextConfig.java
@@ -0,0 +1,23 @@
+package config.servlet;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ * Created by Jacobo on 20/06/2015.
+ */
+
+@Configuration
+@EnableWebMvc
+@ComponentScan("app.controllers")
+public class ServletContextConfig extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry){
+        registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
+    }
+
+}
