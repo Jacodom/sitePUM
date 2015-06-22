14844569e860fffb6f612638cf253410b0203340
Jacobo Dominguez <dominguezjaco@gmail.com>
2015-06-22 16:15:19 -0300

    NegocioController mejorada.

--- src/main/java/backend/app/controller/NegocioController.java
+++ src/main/java/backend/app/controller/NegocioController.java
@@ -1,17 +1,21 @@
 package backend.app.controller;
 
 
+import backend.app.dto.DtoCategoria;
+import backend.app.dto.DtoDetallePedido;
+import backend.app.dto.DtoNegocio;
 import backend.app.model.Categoria;
+import backend.app.model.DetallePedido;
 import backend.app.model.Negocio;
 import backend.app.model.Pedido;
 import backend.app.service.CategoriaService;
 import backend.app.service.NegocioService;
 import backend.app.service.UsuarioService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -29,25 +33,56 @@ public class NegocioController {
     @ResponseBody
     @ResponseStatus(HttpStatus.OK)
     @RequestMapping(value = "/obtenerCategorias", method = RequestMethod.GET)
-    public List<Categoria> obtenerCategorias(){
+    public List<DtoCategoria> obtenerCategorias(){
         List<Categoria> listaCategorias = categoriaService.obtenerCategorias();
-        return listaCategorias;
+        List<DtoCategoria> listaCategoriasDTO = new ArrayList<DtoCategoria>();
+
+        for(Categoria categoria : listaCategorias){
+            DtoCategoria dtoCategoria = new DtoCategoria();
+            dtoCategoria.setIdCategoria(categoria.getIdCategoria());
+            dtoCategoria.setNombreCategoria(categoria.getNombreCategoria());
+            listaCategoriasDTO.add(dtoCategoria);
+        }
+
+        return listaCategoriasDTO;
     }
 
     @ResponseBody
     @ResponseStatus(HttpStatus.OK)
     @RequestMapping(value = "/obtenerNegocios", method = RequestMethod.GET)
-    public List<Negocio> obtenerNegocios(){
+    public List<DtoNegocio> obtenerNegocios(){
         List<Negocio> listaNegocios = negocioService.obtenerNegocios();
-        return listaNegocios;
+        List<DtoNegocio> listaNegociosDTO = new ArrayList<DtoNegocio>();
+
+        for(Negocio negocio : listaNegocios){
+            DtoNegocio dtoNegocio = new DtoNegocio();
+            dtoNegocio.setIdNegocio(negocio.getIdNegocio());
+            dtoNegocio.setDomicilioNegocio(negocio.getDomicilioNegocio());
+            dtoNegocio.setLogoNegocio(negocio.getLogoNegocio());
+            dtoNegocio.setNombreNegocio(negocio.getNombreNegocio());
+            dtoNegocio.setTelefonoNegocio(negocio.getTelefonoNegocio());
+            listaNegociosDTO.add(dtoNegocio);
+        }
+
+        return listaNegociosDTO;
     }
 
     @ResponseBody
     @ResponseStatus(HttpStatus.OK)
     @RequestMapping(value = "/obtenerPedidosUsuario", method = RequestMethod.GET)
-    public List<Pedido> obtenerPedidosUsuario(@RequestBody int idUsuario){
+    public List<DtoDetallePedido> obtenerPedidosUsuario(@RequestBody int idUsuario){
         List<Pedido> listaPedidosUsuario = usuarioService.obtenerPedidosUsuario(idUsuario);
-        return listaPedidosUsuario;
-    }
+        List<DtoDetallePedido> listaDetallesPedidoDto = new ArrayList<DtoDetallePedido>();
 
+        for(Pedido pedido : listaPedidosUsuario){
+            for(DetallePedido detalle : pedido.getDetallePedidos()){
+                DtoDetallePedido detallePedidoDto = new DtoDetallePedido();
+                detallePedidoDto.setNombrePlato(detalle.getPlato().getNombrePlato());
+                detallePedidoDto.setCantidadDetalle(detalle.getCantidadDetalle());
+                listaDetallesPedidoDto.add(detallePedidoDto);
+            }
+        }
+
+        return listaDetallesPedidoDto;
+    }
 }
