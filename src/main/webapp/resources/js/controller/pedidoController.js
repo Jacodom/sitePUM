2cf01504a7617741918c149b71aa1904fecdc20c
Jacobo Dominguez <dominguezjaco@gmail.com>
2015-06-23 03:38:54 -0300

    Parece que funcionan los ajax

--- src/main/webapp/resources/js/controller/pedidoController.js
+++ src/main/webapp/resources/js/controller/pedidoController.js
@@ -1,24 +1,47 @@
 angular.module('sitePumApp',[])
     .controller('pedidoController', ['$scope', '$http', function($scope, $http){
-        $http({
-            method: "GET",
-            url: "/gestionarPedido/obtenerCategoriasNegocio",
-            params: {
-                idNegocio : 1
-            }
-        }).success(function(data){
-            console.log(data);
-            $scope.listaCategorias = data; 
-        });
-        
-        $http({
-            method: "GET",
-            url: "/gestionarPedido/obtenerMenuActivo",
-            params: {
-                idNegocio: 1
-            }
-        }).success(function(data){
-            console.log(data)
-            $scope.menuActivo = data;
-        });
+
+       var obtenerCategoriasNegocio = function ($scope, $http) {
+            $http({
+                method: "GET",
+                url: "/gestionarPedido/obtenerCategoriasNegocio",
+                params: {
+                    idNegocio : 1
+                }
+            }).success(function(data){
+                console.log(data);
+                $scope.listaCategorias = data;
+            });
+        }
+
+        var obtenerMenuActivo = function ($scope, $http) {
+
+            $http({
+                method: "GET",
+                url: "/gestionarPedido/obtenerMenuActivo",
+                params: {
+                    idNegocio: 1
+                }
+            }).success(function(data){
+                console.log(data)
+                $scope.menuActivo = data;
+            });
+        }
+
+        var obtenerPlatosMenu = function ($scope, $http) {
+            $http({
+                method: "GET",
+                url: "/gestionarPedido/obtenerPlatosMenu",
+                params: {
+                    idMenu: 1
+                }
+            }).success(function(data){
+                console.log(data);
+                $scope.platosMenu = data;
+            });
+        }
+
+        obtenerCategoriasNegocio();
+        obtenerMenuActivo();
+        obtenerPlatosMenu();
 }]);
