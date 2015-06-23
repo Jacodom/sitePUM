00a29e9947a10e4c4e4b1efa6489764d3603c10e
Pablo <pablocordoba11@gmail.com>
2015-06-22 22:34:13 -0300

    ControladoraJS

--- src/main/webapp/resources/js/controller/negocioController.js
+++ src/main/webapp/resources/js/controller/negocioController.js
@@ -1,10 +1,19 @@
-angular.module("sitePumApp",[]).controller('negocioController',function($scope){
-    $scope.listaNegocio=[
-        {nombre:'La mira',direccion:'mitre 213' },
+angular.module("sitePumApp",[])
+    .controller('negocioController',function($scope,$http){
+    $scope.listaNegocio=function(){
+        var request = $http({
+        method:'GET',
+        url:'/elegirNegocio/obtenerNegocios',
+        headers:{
+            "Content-Type":"application/json",
+            "Accept":"text/plain, application/json"    
+        }
+    });
+};
+});
+        /*{nombre:'La mira',direccion:'mitre 213' },
         {nombre:'Negocio',direccion:'mitre 213' },
         {nombre:'Negocio 1',direccion:'nacion 45' },
         {nombre:'Negocio 2',direccion:'mitre 213' },
         {nombre:'Negocio 3',direccion:'mitre 213' },
-        
-    ];
-});
+    ];*/
